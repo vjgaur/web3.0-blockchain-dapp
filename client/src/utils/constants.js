6c66274b19217a8e8b31705cd6ef84f74209a0e6
vjgaur <vijayendra.gaur@gmail.com>
2022-01-11 15:51:58 +0530

    deployed smart contract and integrating abi in client

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi;
+export const contractAddress = ' 0x3bF975F5730CCB8d4ded2cd162E665B4259beAe6';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,1 +1,13 @@
 // https://eth-ropsten.alchemyapi.io/v2/ma7EafF9YILi6eYiLdoxaKa2NtOzLnbN
+
+require('@nomiclabs/hardhat-waffle')
+
+module.exports = {
+    solidity: '0.8.0',
+    networks: {
+        ropsten: {
+            url:'https://eth-ropsten.alchemyapi.io/v2/ma7EafF9YILi6eYiLdoxaKa2NtOzLnbN',
+            accounts:['cc8d968485edf5574fab9e1581a73dd185e2b45e568f7259a13782cdf669aeaa']
+        }
+    }
+}
